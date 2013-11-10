3b74110a24707ba037886c7ef13b7c6657ea6aef
张 驰 <ayecool@zhangmatoMacBook-Pro.local>
2013-11-10 22:07:57 +0800

    Create Files

---
+++ HappyPhoto_iOS/HappyPhoto_iOS/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HappyPhoto_iOS
+//
+//  Created by 张 驰 on 13-11-10.
+//  Copyright (c) 2013年 MySense. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
